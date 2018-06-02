61a631cb0a9a7e4ad2eb6a3ab6214258a44e4a1c
Banana Tycoon <39864359+bananatycoon@users.noreply.github.com>
2018-06-02 15:47:18 -0400

    Create game.js

---
+++ game.js
@@ -0,0 +1,24 @@
+//Manual Clicking
+function grow() {
+    bananas = bananas + 1;
+}
+
+//Automatic Stuff
+
+function update() { 
+
+document.title = bananas.toLocaleString() + " Bananas";
+document.getElementById('bananaCount').value = bananas.toLocaleString();
+
+}
+
+function timer() {
+    update();
+}
+
+setInterval(timer, 1000)
+
+// Variables
+var bananas = 0;
+
+
