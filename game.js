d0f82c88c7d6fee0eb154f6e592a1549f678397e
Banana Tycoon <39864359+bananatycoon@users.noreply.github.com>
2018-06-03 12:05:32 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,38 @@
+
+function grow() {
+    bananas = bananas + manualclick;
+    document.getElementById("bananaCount").innerHTML = "Bananas: " + bananas.toLocaleString();
+
+}
+
+function sell20() {
+    if(bananas >= 20) {
+        bananas = bananas - 20;
+        money = money + 5;
+        document.getElementById("moneyCount").innerHTML = "Money: " + "$" + money.toLocaleString();
+        document.getElementById("bananaCount").innerHTML = "Bananas: " +  bananas.toLocaleString();
+
+    }
+}
+
+function sell50() {
+    if(bananas >= 50) {
+        bananas = bananas - 50;
+        money = money + 12;
+        document.getElementById("moneyCount").innerHTML = "Money: " + "$" + money.toLocaleString();
+        document.getElementById("bananaCount").innerHTML = "Bananas: " +  bananas.toLocaleString();
+    }
+}
+
+function enhanceSoil() {
+    if(money >= 50) {
+        money = money - 50;
+        manualclick = manualclick + 5;
+        document.getElementById("moneyCount").innerHTML = "Money: " + "$" + money.toLocaleString();
+        document.getElementById("bananaCount").innerHTML = "Bananas: " +  bananas.toLocaleString();        
+    }
+}
+
+var bananas = 0;
+var money = 0;
+var manualclick = 1;
