17fd70ac239d813fe58a91ab18ddff11428505a9
Banana Tycoon <39864359+bananatycoon@users.noreply.github.com>
2018-06-11 22:22:05 -0400

    Add files via upload

--- game.js
+++ game.js
@@ -1,165 +1,160 @@
+function update() {
+    document.getElementById("bananaCount").innerHTML = "Bananas: " + bananas.toLocaleString();
+    document.getElementById("moneyCount").innerHTML = "Money: $" + money.toLocaleString();
+    document.getElementById("perClickCount").innerHTML = "BPC: " + perClick.toLocaleString();
+    document.getElementById("perSecondCount").innerHTML = "BPS: " + perSecond.toLocaleString();
+    document.getElementById("enhanceSoil").innerHTML = "Price: $" + soilCost.toLocaleString();
+    document.getElementById("purifyWater").innerHTML = "Price: $" + purifyCost.toLocaleString();
+    document.getElementById("expandFarms").innerHTML = "Price: $" + expandCost.toLocaleString();
+    document.getElementById("createBillboards").innerHTML = "Price: $" + billboardCost.toLocaleString();
+    document.getElementById("buySmallerFarm").innerHTML = "Price: $" + buyOutCost.toLocaleString();
+}
+
 // Variables
 
 var bananas = 0;
+var perClick = 1;
+var perSecond = 0;
 var money = 0;
-var manualclick = 1;
-var bananasPerSecond = 0;
-var monkeyCost = 10;
-var gorillaCost = 100;
 
-function prettify(input){
-    var output = Math.round(input * 1000000)/1000000;
-	return output;
-}
+var soilCost = 50;
+var purifyCost = 1000;
+var expandCost = 10000;
+var billboardCost = 100000;
+var buyOutCost = 1000000;
 
 
-function update() {
-    document.getElementById("moneyCount").innerHTML = "Money: " + "$" + money.toLocaleString();
-    document.getElementById("bananaCount").innerHTML = "Bananas: " +  bananas.toLocaleString();
-    document.getElementById("harvestCount").innerHTML = "BPH: " + manualclick.toLocaleString();
-    document.getElementById("bpsCount").innerHTML = "BPS: " + bananasPerSecond.toLocaleString();
-    document.getElementById("hireMonkey").innerHTML = "Monkey - " + "($" + monkeyCost.toLocaleString() + ")";
-    document.getElementById("hireGorilla").innerHTML = "Gorilla - " + "($" + gorillaCost.toLocaleString() + ")";
 
-}
-
-// Save/Load
-
-    function save() {
-        localStorage.setItem("bananas", bananas);
-        localStorage.setItem("manualclick", manualclick);
-        localStorage.setItem("money", money);
-        localStorage.setItem("bananasPerSecond", bananasPerSecond);
-        localStorage.setItem("monkeyCost", monkeyCost);
-        localStorage.setItem("gorillaCost", gorillaCost);
+function save() {
+    localStorage.setItem("bananas", bananas);
+    localStorage.setItem("perClick", perClick);
+    localStorage.setItem("money", money);
     
-    }
+    localStorage.setItem("soilCost", soilCost);
+    localStorage.setItem("purifyCost", purifyCost);
+    localStorage.setItem("expandCost", expandCost);
+    localStorage.setItem("billboardCost", billboardCost);
+    localStorage.setItem("buyOutCost", buyOutCost);
+}
 
-    function load() {
+function load() {
         bananas = localStorage.getItem("bananas");
         bananas = parseInt(bananas);
-        manualclick = localStorage.getItem("manualclick");
-        manualclick = parseInt(manualclick);
+        perClick = localStorage.getItem("perClick");
+        perClick = parseInt(perClick);
         money = localStorage.getItem("money");
         money = parseInt(money); 
-        bananasPerSecond = localStorage.getItem("bananasPerSecond");
-        bananasPerSecond = parseInt(bananasPerSecond);
-        monkeyCost = localStorage.getItem("monkeyCost");
-        monkeyCost = parseInt(monkeyCost);
-        gorillaCost = localStorage.getItem("gorillaCost");
-        gorillaCost = parseInt(gorillaCost);
+        soilCost = localStorage.getItem("soilCost");
+        soilCost = parseInt(soilCost);
+        purifyCost = localStorage.getItem("purifyCost");
+        purifyCost = parseInt(purifyCost);
+        expandCost = localStorage.getItem("expandCost");
+        expandCost = parseInt(expandCost);
+        billboardCost = localStorage.getItem("billboardCost");
+        billboardCost = parseInt(billboardCost);
+        buyOutCost = localStorage.getItem("buyOutCost");
+        buyOutCost = parseInt(buyOutCost);
         update()
-    }
+}
 
-// Manual Clicking
 
-function grow() {
-    bananas = bananas + manualclick;
+//Manual Clicking
+function harvest() {
+    bananas = bananas + perClick;
     update()
 }
 
-// Upgrades
-
+// Sell All lol
 function sellAll() {
     money = money + (bananas * .25);
     bananas = bananas - bananas;
     update()
 }   
 
-function sell20() {
-    if(bananas >= 20) {
+//Sell Bananas
+function sellBundle() {
+    if (bananas >= 20) {
         bananas = bananas - 20;
         money = money + 5;
         update()
     }
 }
 
-function sell50() {
-    if(bananas >= 50) {
+function sellContainer() {
+    if (bananas >= 50) {
         bananas = bananas - 50;
-        money = money + 12;
+        money = money + 15;
         update()
     }
 }
 
-function sell1000() {
-    if(bananas >= 1000) {
+function sellCrate() {
+    if (bananas >= 1000) {
         bananas = bananas - 1000;
-        money = money + 250;
+        money = money + 320;
         update()
     }
 }
 
-function sell10k() {
-   if(bananas >= 10000) {
+function sellShipment() {
+    if (bananas >= 10000) {
         bananas = bananas - 10000;
-        money = money + 3000;
+        money = money + 3500;
         update()
-   }
+    }
 }
 
-function sell100k() {
-    if(bananas >= 100000) {
+function sellSurplus() {
+    if (bananas >= 100000) {
         bananas = bananas - 100000;
-        money = money + 50000;
+        money = money + 40000;
         update()
     }
 }
 
+//Upgrades
 function enhanceSoil() {
-    if(money >= 50) {
-        money = money - 50;
-        manualclick = manualclick + 5;
+    if (money >= soilCost) {
+        money = money - soilCost;
+        perClick = perClick + 5;
+        soilCost = soilCost + 7;
         update()
     }
+
 }
 
 function purifyWater() {
-    if(money >= 200) {
-        money = money - 200;
-        manualclick = manualclick + 25;
+    if (money >= purifyCost) {
+        money = money - purifyCost;
+        perClick = perClick + 50;
+        purifyCost = purifyCost + 128;
         update()
     }
 }
 
 function expandFarms() {
-    if(money >= 10000) {
-        money = money - 10000;
-        manualclick = manualclick + 500;
+    if (money >= expandCost) {
+        money = money - expandCost;
+        perClick = perClick + 500;
+        expandCost = expandCost + 456;
         update()
     }
 }
 
-function hireMarketing() {
-    if(money >= 100000) {
-        money = money - 100000;
-        manualclick = manualclick + 20000;
+function createBillboards() {
+    if (money >= billboardCost) {
+        money = money - billboardCost;
+        perClick = perClick + 8000;
+        billboardCost = billboardCost + 1045;
         update()
     }
 }
 
 function buySmallerFarm() {
-    if(money >= 1000000) {
-        money = money - 1000000;
-        manualclick = manualclick + 75000;
-        update()
-    }
-}
-
-function hireMonkey() {
-    if (money >= monkeyCost) {
-        money = money - monkeyCost;
-        bananasPerSecond = bananasPerSecond + 1;
-        monkeyCost = prettify(monkeyCost * 1.1);
-        update()
-    }
-}
-
-function hireGorilla() {
-    if (money >= gorillaCost) {
-        money = money - gorillaCost;
-        bananasPerSecond = bananasPerSecond + 5;
-        gorillaCost = prettify(gorillaCost * 1.1);
+    if (money >= buyOutCost) {
+        money = money - buyOutCost;
+        perClick = perClick + 75000;
+        buyOutCost = buyOutCost + 13055;
         update()
     }
 }
@@ -167,6 +162,6 @@ function hireGorilla() {
 // Updater 9000
 
 window.setInterval(function(){
-    bananas = bananas + bananasPerSecond;
+  //  bananas = bananas + perSecond;
     update()
 }, 1000);
